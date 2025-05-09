8e0ff5ab3c0a39eb42bb2351ed1858db991f1158
Mofifoluwa Akinwande <153274843+boredfifo@users.noreply.github.com>
2025-05-09 08:23:08 +0200

    Update four_bit_adder_subtractor_2_tb.vhd "cleaner tb"

--- Hardware Engineering/Lab/Lab_01/Fifo/four_bit_adder_subtractor_2_tb.vhd
+++ Hardware Engineering/Lab/Lab_01/Fifo/four_bit_adder_subtractor_2_tb.vhd
@@ -13,18 +13,10 @@ signal a_tb, b_tb, s_tb: BIT_VECTOR (3 downto 0);
 signal k_tb, c_out_tb: BIT;
 
 begin
-DUT5: four_bit_adder_subtractor port map (a(0) => a_tb(0),a(1) => a_tb(1), a(2) => a_tb(2) ,a(3) => a_tb(3), b(0) => b_tb(0),
-b(1) => b_tb(1),b(2) => b_tb(2),b(3) => b_tb(3), S(0) => s_tb(0),S(1) => s_tb(1),S(2) => s_tb(2),S(3) => s_tb(3), K => k_tb, C_OUT => c_out_tb);
+DUT5: four_bit_adder_subtractor port map (a => a_tb, b => b_tb, S=> s_tb, K => k_tb, C_OUT => c_out_tb);
 
-k_tb <= '0', '1' after 10 ns, '0' after 20 ns;
-a_tb(0) <= '0', '1' after 10 ns, '1' after 20 ns;
-a_tb(1) <= '0', '0' after 10 ns, '1' after 20 ns;
-a_tb(2) <= '0', '1' after 10 ns, '0' after 20 ns;
-a_tb(3) <= '0', '0' after 10 ns, '1' after 20 ns;
-b_tb(0) <= '0', '1' after 10 ns, '1' after 20 ns;
-b_tb(1) <= '0', '1' after 10 ns, '1' after 20 ns;
-b_tb(2) <= '0', '0' after 10 ns, '0' after 20 ns;
-b_tb(3) <= '0', '1' after 10 ns, '0' after 20 ns;
-
+k_tb <=    '0', '1' after 10 ns, '0' after 20 ns, '1' after 30 ns;
+a_tb <= "0000", "1011" after 10 ns, "1000" after 20 ns, "0111" after 30 ns;
+b_tb <= "0000", "0011" after 10 ns, "0001" after 20 ns, "0111" after 30 ns;
 
 end structural;
