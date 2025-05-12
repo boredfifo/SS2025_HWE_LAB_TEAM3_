ff29e79e8cd53b8330ff440001a044e8ab7c809d
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-05-13 00:45:12 +0200

    Update ALU_8bit.vhd

--- Hardware Engineering/8 Bit RISC CPU/VHDL/ALU/ALU_8bit.vhd
+++ Hardware Engineering/8 Bit RISC CPU/VHDL/ALU/ALU_8bit.vhd
@@ -17,23 +17,23 @@ BEGIN
 U150: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(0),A=>A(0),EN_A=>EN_A,INV_A=>INV_A,CarryIn=>CarryIn,Output=>Output(0),CarryOut =>carry01 );
 
 
-U151: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(1),A=>A(1),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(1),CarryOut =>carry12 , CarryIn=>'0');
+U151: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(1),A=>A(1),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(1),CarryOut =>carry12 , CarryIn=>carry01);
 
 
-U152: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(2),A=>A(2),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(2),CarryOut =>carry23 , CarryIn=>'0');
+U152: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(2),A=>A(2),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(2),CarryOut =>carry23 , CarryIn=>carry12);
 
-U153: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(3),A=>A(3),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(3),CarryOut =>carry34, CarryIn=>'0' );
-
-
-U154: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(4),A=>A(4),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(4),CarryOut =>carry45, CarryIn=>'0' );
+U153: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(3),A=>A(3),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(3),CarryOut =>carry34, CarryIn=>carry23 );
 
 
-U155: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(5),A=>A(5),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(5),CarryOut =>carry56, CarryIn=>'0' );
+U154: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(4),A=>A(4),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(4),CarryOut =>carry45, CarryIn=>carry34 );
 
 
-U156: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(6),A=>A(6),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(6),CarryOut =>carry67, CarryIn=>'0');
+U155: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(5),A=>A(5),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(5),CarryOut =>carry56, CarryIn=>carry45 );
 
 
-U157: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(7),A=>A(7),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(7),CarryOut =>CarryOut, CarryIn=>'0' );
+U156: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(6),A=>A(6),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(6),CarryOut =>carry67, CarryIn=>carry56);
+
+
+U157: ALU_1BIT PORT MAP(F0=>F0, F1=>F1, EN_B=>EN_B, B=>B(7),A=>A(7),EN_A=>EN_A,INV_A=>INV_A,Output=>Output(7),CarryOut =>CarryOut, CarryIn=>carry67 );
 
 END structural;
