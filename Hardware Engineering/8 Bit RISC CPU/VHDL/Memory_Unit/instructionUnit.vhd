ee163cee4a48ef1a3cb1074809864c176d6d9c3a
Mofifoluwa Akinwande <153274843+boredfifo@users.noreply.github.com>
2025-05-16 09:50:28 +0200

    Update instructionUnit.vhd

--- Hardware Engineering/8 Bit RISC CPU/VHDL/Memory_Unit/instructionUnit.vhd
+++ Hardware Engineering/8 Bit RISC CPU/VHDL/Memory_Unit/instructionUnit.vhd
@@ -11,13 +11,13 @@ architecture structural of instruction_memoryUnit is
 begin
 with pcAddress select                     --INSTRUCTION SET
 instruction <= "00001000" when "00000001", --LOAD
-		"00010001" when "00000010", --STORE
-		"00011010" when "00000011", --ADD
-		"00100011" when "00000100", --SUB
-		"00101100" when "00000101", --IN
-		"00110100" when "00000110", --OUT
-		"00111101"  when "00000111", --JZ
-		"01000110" when "00001000", --JPOS
-		"01001111" when "00001001", --J
+		"00110001" when "00000010", --STORE
+		"01011010" when "00000011", --ADD
+		"01100011" when "00000100", --SUB
+		"10001100" when "00000101", --IN
+		"10010100" when "00000110", --OUT
+		"10111101"  when "00000111", --JZ
+		"11000110" when "00001000", --JPOS
+		"11101111" when "00001001", --J
 		"00000000" when others;
 end structural;
