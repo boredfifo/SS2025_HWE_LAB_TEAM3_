9280d3ecad116a846b2a683a9fcfe1df96a5eb5d
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-05-16 08:12:46 +0200

    Update instructionRegister.vhd

--- Hardware Engineering/8 Bit RISC CPU/VHDL/InstructionRegister/instructionRegister.vhd
+++ Hardware Engineering/8 Bit RISC CPU/VHDL/InstructionRegister/instructionRegister.vhd
@@ -15,19 +15,16 @@ SIGNAL OPCode: STD_LOGIC_VECTOR(2 downto 0);
 SIGNAL Operand: STD_LOGIC_VECTOR(4 downto 0);
 BEGIN
 PROCESS(ControlClock, Loader, ControlReset)
-VARIABLE tempOPCode: STD_LOGIC_VECTOR(2 downto 0);
-VARIABLE tempOperand: STD_LOGIC_VECTOR(4 downto 0);
 BEGIN
 if ControlReset = '1' then
-AddressFromInstructionMemory <= "00000000";
+OPCode <= "000";
+Operand<="00000";
 elsif rising_edge(ControlClock) then
 	if Loader = '1' then
-	tempOPCode := AddressFromInstructionMemory(7 downto 5);
-	tempOperand := AddressFromInstructionMemory(4 downto 0);
+	OPCode := AddressFromInstructionMemory(7 downto 5);
+	Operand := AddressFromInstructionMemory(4 downto 0);
 	end if;
 end if;
-OPCode<=tempOPCode;
-Operand<=tempOperand;
 end process;
 
 OpCodeForControlUnit<=OPCode;
