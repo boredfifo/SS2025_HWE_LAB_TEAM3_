bb7d8f74719396b843caf157013a91011e3010a0
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-05-18 22:09:27 +0200

    Update Accumulator.vhd

--- Hardware Engineering/8 Bit RISC CPU/VHDL/ACCUMULATOR/Accumulator.vhd
+++ Hardware Engineering/8 Bit RISC CPU/VHDL/ACCUMULATOR/Accumulator.vhd
@@ -12,7 +12,10 @@ PORT(
 	carryBit: IN STD_LOGIC;
 
 	dataintoALU: OUT STD_LOGIC_VECTOR(7 downto 0);
-	positiveFlag, ZeroFlag, carryFlag: OUT STD_LOGIC);
+	flagEnablerFromControlUnit: IN STD_LOGIC;
+	negativeFlag, ZeroFlag, carryFlag: OUT STD_LOGIC);
+
+	
 END Accumulator;
 
 ARCHITECTURE BEHAVIOURAL Of Accumulator IS
@@ -21,38 +24,42 @@ SIGNAL accumulatorDecode: STD_LOGIC_VECTOR(7 downto 0);
 BEGIN
 
 PROCESS(clock, reset)
-VARIABLE temporaryVariable: STD_LOGIC_VECTOR(7 downto 0);
 BEGIN
 	if reset = '1' then
 		accumulatorDecode <= (OTHERS => '0');
 	elsif rising_edge(clock) then
 		IF load = '1' then
                 	CASE sourceSelector is
-                    		WHEN "00" => temporaryVariable := datafromALU;
-                    		WHEN "01" => temporaryVariable := dataFromMemory;
-                    		WHEN "10" => temporaryVariable := dataFromUser;
-                    		WHEN OTHERS => temporaryVariable := (OTHERS => '0');
+                    		WHEN "00" => accumulatorDecode <= datafromALU;
+                    		WHEN "01" => accumulatorDecode <= dataFromDataUnit;
+                    		WHEN "10" => accumulatorDecode <= dataFromUser;
+                    		WHEN OTHERS => accumulatorDecode <= (OTHERS => '0');
                 	END CASE;
            	 	
-			accumulatorDecode <= temporaryVariable;
-				
-		END IF;
+			END IF;
 	
 	END IF;
 END PROCESS;
 		dataintoALU <= accumulatorDecode;
 
-WITH accumulatorDecode SELECT
-		ZeroFlag <= '1' WHEN "00000000",
-			    '0' WHEN OTHERS;
+PROCESS(accumulatorDecode, carryBit, flagEnablerFromControlUnit)
+BEGIN
+	IF flagEnablerFromControlUnit = '1' then
+		IF accumulatorDecode = "00000000" THEN
+			ZeroFlag <= '1';
+		ELSE
+			ZeroFlag <= '0';
+		END IF;
 
-WITH accumulatorDecode(7) SELECT
-		positiveFlag <= '1' WHEN '0',
-				'0' WHEN OTHERS;
+		negativeFlag <= accumulatorDecode(7); 
 
-WITH carryBit SELECT
-		carryFlag <= '1' WHEN '1',
-				'0' WHEN OTHERS;
+		carryFlag <= carryBit;
+	ELSE
+		ZeroFlag <= '0';
+		negativeFlag <= '0';
+		carryFlag <= '0';
+	END IF;
+END PROCESS;
 			
 		
 END BEHAVIOURAL;
