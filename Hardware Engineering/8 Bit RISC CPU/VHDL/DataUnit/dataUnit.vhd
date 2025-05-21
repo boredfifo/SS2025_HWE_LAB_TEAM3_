e793df5742079786b9158b6d1b73d95126fb0c30
Mofifoluwa Akinwande <153274843+boredfifo@users.noreply.github.com>
2025-05-21 15:00:57 +0200

    Update dataUnit.vhd --new data

--- Hardware Engineering/8 Bit RISC CPU/VHDL/DataUnit/dataUnit.vhd
+++ Hardware Engineering/8 Bit RISC CPU/VHDL/DataUnit/dataUnit.vhd
@@ -12,8 +12,13 @@ end data_memoryUnit;
 architecture behavioral of data_memoryUnit is
 type ram_array is array(0 to 31) of STD_LOGIC_VECTOR(7 downto 0);
 signal data_memory : ram_array := (
- 
-  others => (others => '0') 
+  0 => "00000100",  -- address 0
+  1 => "00000011",  -- address 1
+  2 => "00000010",  -- address 2
+  3 => "00000000",  -- address 3
+  4 => "00000001",  -- address 4
+  5 => "11111111", -- address 5
+  others => (others => '0')
 );
 begin
 process(CLK)
