e643c2e8351610c59ea8527f830202c944b7df80
Mofifoluwa Akinwande <153274843+boredfifo@users.noreply.github.com>
2025-05-21 15:02:23 +0200

    Update instructionUnit.vhd --instruction to count from 0 to 255

--- Hardware Engineering/8 Bit RISC CPU/VHDL/Memory_Unit/instructionUnit.vhd
+++ Hardware Engineering/8 Bit RISC CPU/VHDL/Memory_Unit/instructionUnit.vhd
@@ -10,14 +10,12 @@ end instruction_memoryUnit;
 architecture structural of instruction_memoryUnit is
 begin
 with pcAddress select                     --INSTRUCTION SET
-instruction <= "00001000" when "00000001", --LOAD
-		"00110001" when "00000010", --STORE
-		"01011010" when "00000011", --ADD
-		"01100011" when "00000100", --SUB
-	--	"10001100" when "00000101", --IN
-		"10010100" when "00000110", --OUT
-		"10111101"  when "00000111", --JZ
-		"11000110" when "00001000", --JPOS
-		"11101111" when "00001001", --J
+instruction <= "00000011" when "00000001", --LOAD 3
+		"01000100" when "00000010", --ADD 4
+		"00100011" when "00000011", --STORE 3
+		"01100101" when "00000100", --SUB 5
+		"10000000" when "00000101", --JZ 0
+		"11000001" when "00000110", --JUMP 1
 		"00000000" when others;
 end structural;
+	
