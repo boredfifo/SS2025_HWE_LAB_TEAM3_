c1e0987f642aa23bcdc79390393ff8b801771254
Mofifoluwa Akinwande <153274843+boredfifo@users.noreply.github.com>
2025-05-28 17:29:21 +0200

    Update brokerScript.py #2

--- Advanced Embedded Engineering/Project/solorproject_with_MQTT_RP_/brokerScript.py
+++ Advanced Embedded Engineering/Project/solorproject_with_MQTT_RP_/brokerScript.py
@@ -2,12 +2,13 @@ import paho.mqtt.subscribe as subscribe
 import paho.mqtt.publish as publish
 import json
 import time
+import paho.mqtt.client as mqtt
 
 threshold = 100
-hostName = "192.168.1.100"
+#hostName = "192.168.1.100"
 
 def getValues():
-    msg = subscribe.simple("solartracker/ldr", hostname=hostName)
+    msg = subscribe.simple("solartracker/ldr")
     ldrValuesfromBroker = msg.payload.decode()
     parsedLDRvalues = json.loads(ldrValuesfromBroker)
     return parsedLDRvalues
@@ -25,6 +26,8 @@ def publishbRecommendation():
         status = "bright"
     publish.single("ldr/control/status", status, hostname=hostName)
 
+client = mqtt.Client()
+client.connect("localhost", 1883, 60)
 while True:
     publishbRecommendation()
     time.sleep(5)
