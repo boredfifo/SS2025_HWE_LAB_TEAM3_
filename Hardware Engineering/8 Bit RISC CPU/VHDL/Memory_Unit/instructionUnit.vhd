fe57ac82b9adfe2f5d0f6b1739cb6225335c104b
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-05-30 00:22:21 +0200

    Update instructionUnit.vhd

--- Hardware Engineering/8 Bit RISC CPU/VHDL/Memory_Unit/instructionUnit.vhd
+++ Hardware Engineering/8 Bit RISC CPU/VHDL/Memory_Unit/instructionUnit.vhd
@@ -9,14 +9,88 @@ end instruction_memoryUnit;
 
 architecture structural of instruction_memoryUnit is
 begin
-with pcAddress select                     --INSTRUCTION SET
-instruction <=  "10000000" when "00000001", --NO OP 3
-		"00000011" when "00000010", --LOAD 3
---		"01000100" when "00000011", --ADD 4
---		"00100011" when "00000100", --STORE 3
---		"01100101" when "00000101", --SUB 5
---		"10000000" when "00000110", --JZ 0
---		"11000001" when "00000111", --JUMP 1
-		"10000000" when others;
+
+process(pcAddress)
+begin
+		CASE(pcAddress) IS
+				WHEN "00000000" => instruction<="00000000"; 
+				WHEN "00000001" => instruction<="10011110";	--Load 250 from address 30
+				WHEN "00000010" => instruction<="01000001";	--Add 1
+				WHEN "00000011" => instruction<="00111110";	--Store result where 250 was stored
+				WHEN "00000100" => instruction<="11000001";	--Jump	to program counter 00000001 if no carry flag
+				WHEN "00000101" => instruction<="10001010";	--Load 10
+            	WHEN OTHERS => instruction <= "00000000"; -- No Operation
+		END CASE;
+
+END PROCESS;
+
 end structural;
+
+--		CASE(pcAddress) IS
+--				WHEN "00000000" => instruction<="00000000"; 
+--				WHEN "00000001" => instruction<="10011111";	--Load 255 from address 31
+--				WHEN "00000010" => instruction<="01000001";	--Add 1
+--				WHEN "00000011" => instruction<="11000001";	--Jump	to program counter 00000001 if no carry flag
+--				WHEN "00000100" => instruction<="10001010";	--Load 10
+--            	WHEN OTHERS => instruction <= "00000000"; -- No Operation
+--		END CASE;
+
+--		CASE(pcAddress) IS
+--				WHEN "00000000" => instruction<="00000000"; 
+--				WHEN "00000001" => instruction<="10011110";	--Load 250 from address 30
+--				WHEN "00000010" => instruction<="01000001";	--Add 1
+--				WHEN "00000011" => instruction<="11000001";	--Jump	to program counter 00000001 if no carry flag
+--				WHEN "00000100" => instruction<="10001010";	--Load 10
+--            	WHEN OTHERS => instruction <= "00000000"; -- No Operation
+--		END CASE;
+
+--		CASE(pcAddress) IS
+--				WHEN "00000000" => instruction<="00000000"; 
+--				WHEN "00000001" => instruction<="10000100";	--Load	
+--				WHEN "00000010" => instruction<="00100011";	--Store
+--				WHEN "00000011" => instruction<="10000011";	--Load
+--            			WHEN OTHERS => 
+--                		instruction <= "00000000"; -- No Operation
+--		END CASE;
 	
+
+--		CASE(pcAddress) IS
+--				WHEN "00000000" => instruction<="00000000"; 
+--				WHEN "00000001" => instruction<="10000110";	--Load	
+--				WHEN "00000010" => instruction<="01000011";	--Add
+--				WHEN "00000011" => instruction<="00100000";	--Store					
+--				WHEN "00000100" => instruction<="10000000";	--Load				
+--            			WHEN OTHERS => 
+--                		instruction <= "00000000";
+--		END CASE;
+
+--		CASE(pcAddress) IS
+--				WHEN "00000000" => instruction<="00000000"; 
+--				WHEN "00000001" => instruction<="10000110";	--Load 6	
+--				WHEN "00000010" => instruction<="01100001";	--Sub  1
+--				WHEN "00000011" => instruction<="00100011";	--Store
+--				WHEN "00000100" => instruction<="10000011";	--Load				
+--            			WHEN OTHERS => 
+--                		instruction <= "00000000";
+--		END CASE;
+		
+--				CASE(pcAddress) IS
+--				WHEN "00000000" => instruction<="00000000"; 
+--				WHEN "00000001" => instruction<="10000110";	--Load	
+--				WHEN "00000010" => instruction<="11100011";	--AND
+--				WHEN "00000011" => instruction<="00100011";	--Store
+--				WHEN "00000100" => instruction<="10000011";	--Load				
+--            			WHEN OTHERS => 
+--                		instruction <= "00000011";
+--		END CASE;
+
+
+--		CASE(pcAddress) IS
+--				WHEN "00000000" => instruction<="00000000"; 
+--				WHEN "00000001" => instruction<="10000110";	--Load	
+--				WHEN "00000010" => instruction<="10100011";	--OR
+--				WHEN "00000011" => instruction<="00100011";	--Store
+--				WHEN "00000100" => instruction<="10000011";	--Load				
+--            			WHEN OTHERS => 
+--                		instruction <= "00000011";
+--		END CASE;
