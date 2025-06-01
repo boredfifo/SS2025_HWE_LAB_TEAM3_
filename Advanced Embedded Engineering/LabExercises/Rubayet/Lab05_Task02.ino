dcbe445a7146c7aed8e340b9c4cdfadda274a7be
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-06-01 11:26:15 +0200

    Create Lab05_Task02.ino

---
+++ Advanced Embedded Engineering/LabExercises/Rubayet/Lab05_Task02.ino
@@ -0,0 +1,4 @@
+Use the following tutorial to use your Raspberry Pi as now as MQTT client.
+https://randomnerdtutorials.com/testing-mosquitto-broker-and-client-on-raspbberypi/
+For this you will use different terminals as publisher and subscriber using the
+mosquitto mqtt broker.
