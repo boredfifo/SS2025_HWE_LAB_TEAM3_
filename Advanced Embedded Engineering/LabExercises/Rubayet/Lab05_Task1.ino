66d55fa3491cebebd14a00cbc7db6a005ec27d81
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-06-01 11:25:49 +0200

    Create Lab05_Task1.ino

---
+++ Advanced Embedded Engineering/LabExercises/Rubayet/Lab05_Task1.ino
@@ -0,0 +1,5 @@
+• The Mosquitto Broker can be downloaded here: https://mosquitto.org/
+Use the following tutorial to use your Raspberry Pi as MQTT broker. Make sure you
+have completed Lab 03 before, installing the raspberry pi OS on your raspberry Pi 3
+board.
+https://randomnerdtutorials.com/how-to-install-mosquitto-broker-on-raspberry-pi/
