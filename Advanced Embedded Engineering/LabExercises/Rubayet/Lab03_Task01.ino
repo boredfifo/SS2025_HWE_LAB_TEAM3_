3bebf0cba26f3ad68881dd3886f4e46b03ee77de
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-06-01 11:24:27 +0200

    Create Lab03_Task01.ino

---
+++ Advanced Embedded Engineering/LabExercises/Rubayet/Lab03_Task01.ino
@@ -0,0 +1,7 @@
+• In this part of the Lab you will use your Arduino Uno Wifi Rev 2 as MQTT Client for
+sending your Sensor Data to a MQTT Broker.
+• You will need to you use the following libraries:
+• ArduinoMqttClient - https://github.com/arduino-libraries/ArduinoMqttClient
+• WIFININA - https://www.arduino.cc/reference/en/libraries/wifinina/
+• Use the Tutorial:
+https://docs.arduino.cc/tutorials/uno-wifi-rev2/uno-wifi-r2-mqtt-device-to-device
