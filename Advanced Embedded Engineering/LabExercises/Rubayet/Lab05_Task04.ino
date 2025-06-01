a4f753a099778d1dfebef96b8ac1e66cb8240757
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-06-01 11:27:11 +0200

    Create Lab05_Task04.ino

---
+++ Advanced Embedded Engineering/LabExercises/Rubayet/Lab05_Task04.ino
@@ -0,0 +1,5 @@
+In this part of the Lab you will connect all of your sensor nodes and actuator nodes
+(Arduino Uno Wifi Rev 2, ESP32, smartphone app, etc.) as MQTT Clients for sending
+your sensor Data to a MQTT Broker as publisher and receiving on actuator side as
+subscriber.
+Hint: Use the sensors and actuators from your project
