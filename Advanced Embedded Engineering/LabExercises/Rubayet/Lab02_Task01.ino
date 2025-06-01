d5fd900d2f534344916386e455e41dd226ad15af
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-06-01 11:23:06 +0200

    Create Lab02_Task01.ino

---
+++ Advanced Embedded Engineering/LabExercises/Rubayet/Lab02_Task01.ino
@@ -0,0 +1,9 @@
+Arduino Wifi and Sensors
+• Download the Sensor Kit X-40 data sheet:
+https://joy-it.net/files/files/Produkte/SEN-Kit02/SEN-KitX40-Datasheet.pdf
+• Visit the following link for additional information:
+• https://sensorkit.joy-it.net/en/
+• Connect the temperature sensor of the sensor kit X40 to your Arduino Uno Wifi and
+send the sensor data to a web server.
+• Select the sensors related to your project/concept and connect them to your
+Arduino Uni Wifi. Think about a concept how to read, send and evaluate your data
