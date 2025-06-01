8c6d769b11f2e1b523276ac7d1588953e1fab255
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-06-01 11:27:43 +0200

    Create Lab06_Task1.ino

---
+++ Advanced Embedded Engineering/LabExercises/Rubayet/Lab06_Task1.ino
@@ -0,0 +1,4 @@
+Use the following tutorial to use connect your Raspberry Pi to the adafruit IO cloud.
+Based on the example in the tutorial you should be able to control an LED and a Push
+button from the internet based on the Adafruit IO platform
+https://www.woolseyworkshop.com/2019/05/31/adafruit-io-connecting-yourraspberry-pi-to-the-outside-world/
