6695e729b42c0fb6342d4c546cb05c349a094677
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-07-13 14:03:23 +0200

    Create ClockDivider.vhd

---
+++ Hardware Engineering/Lab/Lab_06/Rubayet/ClockDivider.vhd
@@ -0,0 +1,32 @@
+ENTITY ClockDivider IS
+    generic (
+        N : natural := 4  -- Divide factor (must be ≥ 2)
+    );
+    port (
+        Clock    : in  bit;
+        RESET    : in  bit;
+        CLOCK_N  : out bit
+    );
+END ENTITY ClockDivider;
+
+ARCHITECTURE Behavioral OF ClockDivider IS
+    SIGNAL count : natural := 0;
+    SIGNAL temporary_Clock : bit := '0';
+BEGIN
+    PROCESS (Clock, RESET)
+    BEGIN
+        IF RESET = '1' THEN
+            count <= 0;
+            temporary_Clock <= '0';
+        ELSIF Clock = '1' and Clock'event THEN  -- Rising edge detection
+            IF count = (N / 2) - 1 THEN
+                temporary_Clock <= not temporary_Clock;
+                count <= 0;
+            else
+                count <= count + 1;
+            END IF;
+        END IF;
+    END PROCESS;
+
+    CLOCK_N <= temporary_Clock;
+END ARCHITECTURE Behavioral;
