270e24ce51f7f15f573b8bcbe6d2aa7e0828dd48
Moiz Zaheer Malik <153621024+moizZM@users.noreply.github.com>
2025-07-16 21:54:30 +0200

    Add files via upload

---
+++ Advanced Embedded Engineering/LabExercises/Moiz/lab 6 .ino
@@ -0,0 +1,8 @@
+Use the following tutorial to connect your Raspberry Pi as a MQTT broker (Mosquitto)
+to the Adafruit IO platform
+https://learn.adafruit.com/manually-bridging-mqtt-mosquitto-to-adafruit-io/
+
+
+Task 2 – MQTT Bridging Adafruit IO
+Read the tutorial carefully before you start implementing it. Replace the sensor
+application based on the ESP with your sensor application based on arduino Uno Wifi
---
+++ Advanced Embedded Engineering/LabExercises/Moiz/labt3 task2.ino
@@ -0,0 +1,102 @@
+#include <ArduinoMqttClient.h>
+#include <WiFiNINA.h>
+#include "arduino_secrets.h"
+
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "test.mosquitto.org";
+int        port     = 1883;
+const char topic[]  = "LightIntensity";
+
+//set interval for sending messages (milliseconds)
+const long interval = 8000;
+unsigned long previousMillis = 0;
+
+int count = 0;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // attempt to connect to Wifi network:
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+}
+
+void loop() {
+
+  mqttClient.poll();
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    //record random value from A0, A1 and A2
+    int LDR1 = analogRead(A0);
+    int LDR2 = analogRead(A1);
+    int LDR3 = analogRead(A2);
+    int LDR4 = analogRead(A4);
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(topic);
+    Serial.println(LDR1);
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(topic);
+    Serial.println(LDR2);
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(topic);
+    Serial.println(LDR3);
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(topic);
+    Serial.println(LDR4);
+
+    
+mqttClient.beginMessage("LightIntensity");
+mqttClient.print("{\"LDR1\": ");
+mqttClient.print(LDR1);
+mqttClient.print(", \"LDR2\": ");
+mqttClient.print(LDR2);
+mqttClient.print(", \"LDR3\": ");
+mqttClient.print(LDR3);
+mqttClient.print(", \"LDR4\": ");
+mqttClient.print(LDR4);
+mqttClient.print("}");
+mqttClient.endMessage();
+
+
+    Serial.println();
+  }
+}
