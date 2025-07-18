7d68f1157d518680559b0e7996f402569868560e
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-07-18 10:52:16 +0200

    Update UpCounter_3Bit.vhd

--- Hardware Engineering/Lab/Lab_05/Rubayet/UpCounter_3Bit.vhd
+++ Hardware Engineering/Lab/Lab_05/Rubayet/UpCounter_3Bit.vhd
@@ -1,38 +1,62 @@
-ENTITY 3BIT_UP_COUNTER is
-PORT(
-        clock: IN BIT;
-        OutBits: OUT BIT_VECTOR(2 downto 0)
+ENTITY 3BIT_UP_COUNTER IS
+    PORT(
+        clock : IN BIT;
+        OutBits : OUT BIT_VECTOR(2 DOWNTO 0)
     );
 END 3BIT_UP_COUNTER;
 
-ARCHITECTURE BEHAVIOURAL of 3BIT_UP_COUNTER is
+ARCHITECTURE BEHAVIOURAL OF 3BIT_UP_COUNTER IS
 
-COMPONENT JK_FLIPFLOP IS
-  PORT(
-    J, K, clock, reset, enabler : IN BIT;
-    output, notOutput : OUT BIT
-  );
-END COMPONENT;
+    COMPONENT JK_FLIPFLOP IS
+        PORT(
+            J, K, clock, reset, enabler : IN BIT;
+            output, notOutput : OUT BIT
+        );
+    END COMPONENT;
 
-signal LSB, MiddleBIT, MSB, j0, k0, j1, k1, j2: BIT;
+    SIGNAL q0, q1, q2 : BIT;
+    SIGNAL j0, k0, j1, k1, j2, k2 : BIT;
+    SIGNAL reset, enabler : BIT := '1';
 
 BEGIN
-j0 <= not q0;
-k0 <= q0;
 
- 
-j1 <= q0 and not q1;
-k1 <= q0 and q1;
 
-j2 <= q0 and q1;  
+    j0 <= '1';
+    k0 <= '1';
 
-    
-DUT1: JK_FLIPFLOP port map(J => j0, K => k0, CLK => CLK, Q => q0, Q_N => open);
-DUT2: JK_FLIPFLOP port map(J => j1, K => k1, CLK => CLK, Q => q1, Q_N => open);
-DUT3: JK_FLIPFLOP port map(J => j2, K => j2, CLK => CLK, Q => q2, Q_N => open);
+    j1 <= q0;
+    k1 <= q0;
 
-   
-output <= q2 & q1 & q0;
+    j2 <= q0 AND q1;
+    k2 <= q0 AND q1;
+
+    DUT1: JK_FLIPFLOP PORT MAP(
+        J => j0, K => k0,
+        clock => clock,
+        reset => reset,
+        enabler => enabler,
+        output => q0,
+        notOutput => OPEN
+    );
+
+    DUT2: JK_FLIPFLOP PORT MAP(
+        J => j1, K => k1,
+        clock => clock,
+        reset => reset,
+        enabler => enabler,
+        output => q1,
+        notOutput => OPEN
+    );
+
+    DUT3: JK_FLIPFLOP PORT MAP(
+        J => j2, K => k2,
+        clock => clock,
+        reset => reset,
+        enabler => enabler,
+        output => q2,
+        notOutput => OPEN
+    );
+
+    OutBits <= q2 & q1 & q0;
 
 END BEHAVIOURAL;
-
